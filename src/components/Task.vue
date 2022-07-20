dab305f10114d415368551a28035e1e7ed2c6dcf
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-20 20:52:58 +0100

    Task component

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,11 @@
 <template>
-  <h3>{{ task.title }}</h3>
+  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.title }}
+      <i class="fa-solid fa-trash-can"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
 </template>
 
 <script>
@@ -12,5 +18,24 @@ export default {
 </script>
 
 <style scoped>
+.fa-solid {
+  color: #EB1D36;
+}
 
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid #3CCF4E;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
